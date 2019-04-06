40476ab770b02f9389747aba542008c573a61721
lianggx <1104695389@qq.com>
2019-04-07 00:48:51 +0800

    clean up some data

--- data_process.py
+++ data_process.py
@@ -77,11 +77,11 @@ def process_param5():
     for fp in os.listdir(base_path):
         file_path = os.path.join(base_path, fp)
         df = pd.read_csv(file_path, index_col=0)
-        df["long_rate"] = df["long_rate"].apply(lambda x:float(x.replace("%", ""))/100)
+        df["long_rate"] = df["long_rate"].apply(lambda x: float(x.replace("%", "")) / 100)
         df["short_rate"] = df["short_rate"].apply(lambda x: float(x.replace("%", "")) / 100)
         df["close_rate"] = df["close_rate"].apply(lambda x: float(x.replace("%", "")) / 100)
-        df["trade_rate"] = df["trade_rate"].apply(lambda x: float(re.search(r"\d+(\.\d+)?", x).group(0))/10000)
-        df["settle_rate"] = df["settle_rate"].apply(lambda x: float(re.search(r"\d+(\.\d+)?", x).group(0))/10000)
+        df["trade_rate"] = df["trade_rate"].apply(lambda x: float(re.search(r"\d+(\.\d+)?", x).group(0)) / 10000)
+        df["settle_rate"] = df["settle_rate"].apply(lambda x: float(re.search(r"\d+(\.\d+)?", x).group(0)) / 10000)
         df.to_csv(file_path)
 
 
@@ -89,7 +89,7 @@ def process_param6():
     base_path = r"data_files\future\param"
     for fp in os.listdir(base_path):
         file_path = os.path.join(base_path, fp)
-        new_fp = datetime.strptime(fp[:8],"%Y%m%d").strftime("%Y-%m-%d.csv")
+        new_fp = datetime.strptime(fp[:8], "%Y%m%d").strftime("%Y-%m-%d.csv")
         os.rename(file_path, os.path.join(base_path, new_fp))
 
 
@@ -131,6 +131,57 @@ def process_settle():
     print(df)
 
 
+def process_future_quote():
+    quote_path = r"data_files\future\quote"
+    info_path = r"data_files\index\HS300_index.csv"
+    store_path = r"data_files\temp\%s.csv"
+    info = pd.read_csv(info_path, index_col=0)
+    for date in info.index:
+        df = pd.DataFrame()
+        index_list = []
+        for quote_file in os.listdir(quote_path):
+            quote_file_path = os.path.join(quote_path, quote_file)
+            df_t = pd.read_csv(quote_file_path, index_col=0)
+            df_t = df_t.loc[(df_t.index == date) & (df_t["close"].notnull())]
+            if not df_t.empty:
+                df = df.append(df_t.loc[df_t.index == date], ignore_index=True)
+                index_list.append(quote_file[:6])
+        df.index = index_list
+        df.to_csv(store_path % date)
+
+
+def process_bonus1():
+    info_path = r"data_files\index\HS300_index.csv"
+    bonus_path = r"data_files\stock\bonus"
+    store_path = r"data_files\temp\%s.csv"
+    info = pd.read_csv(info_path, index_col=0)
+    for date in info.index:
+        if date < "2018-06-07":
+            continue
+        df = pd.DataFrame()
+        index_list = []
+        print(date)
+        for bonus_file in os.listdir(bonus_path):
+            bonus_file_path = os.path.join(bonus_path, bonus_file)
+            df_t = pd.read_csv(bonus_file_path, index_col=0)
+            df_t = df_t.loc[df_t["a_bonus_date"] == date]
+            if not df_t.empty:
+                df = df.append(df_t)
+                index_list.append(bonus_file[:6])
+        try:
+            df.index = index_list
+        except ValueError:
+            print(index_list)
+            break
+        df = df.dropna(1)
+        if not df.empty:
+            df.to_csv(store_path % date)
+
+
+def process_bonus2():
+    pass
+
+
 if __name__ == "__main__":
     # process_date()
     # process_param2()
@@ -141,4 +192,6 @@ if __name__ == "__main__":
     # process_param5()
     # process_future_info_date()
     # process_param6()
-    process_settle()
+    # process_settle()
+    # process_future_quote()
+    process_bonus1()
